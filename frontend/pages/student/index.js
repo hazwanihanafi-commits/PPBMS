a1fc4c77c25189c335de1f802303423a14ebad74
hazwanihanafi-commits <hazwanihanafi@gmail.com>
2025-11-25 22:50:09 +0800

    Update index.js

--- frontend/pages/student/index.js
+++ frontend/pages/student/index.js
@@ -1,3 +1,12 @@
-export default function StudentPage() {
-  return <div>Student timeline page</div>;
+import { useEffect } from "react";
+import { useRouter } from "next/router";
+
+export default function StudentIndex() {
+  const router = useRouter();
+
+  useEffect(() => {
+    router.replace("/student/me");
+  }, []);
+
+  return <div>Loading…</div>;
 }
