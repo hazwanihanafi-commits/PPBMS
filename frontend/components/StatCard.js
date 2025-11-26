bbf169fbb8d1d6ace0b17508d243dd7d9c73ae34
hazwanihanafi-commits <hazwanihanafi@gmail.com>
2025-11-26 20:52:06 +0800

    Delete frontend/components/StatCard.js

--- frontend/components/StatCard.js
+++
@@ -1,9 +0,0 @@
-// frontend/components/StatCard.js
-export default function StatCard({ title, value }) {
-  return (
-    <div className="bg-white rounded-2xl shadow p-5 flex flex-col justify-between">
-      <div className="text-sm font-medium text-gray-500">{title}</div>
-      <div className="mt-3 text-2xl font-bold text-gray-800">{value}</div>
-    </div>
-  );
-}
