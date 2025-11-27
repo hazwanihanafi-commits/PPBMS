fdfd4bee446adcdde46cde9d1ad5796a19a9268d
hazwanihanafi-commits <hazwanihanafi@gmail.com>
2025-11-27 13:48:09 +0800

    Update GradientCard.jsx

--- frontend/components/GradientCard.jsx
+++ frontend/components/GradientCard.jsx
@@ -1,10 +1,8 @@
 // components/GradientCard.jsx
 export default function GradientCard({ children }) {
   return (
-    <div className="bg-white rounded-lg shadow-sm border border-transparent overflow-hidden">
-      <div className="p-5">
-        {children}
-      </div>
+    <div className="rounded-lg bg-white shadow-sm p-6 border border-gray-100">
+      {children}
     </div>
   );
 }
