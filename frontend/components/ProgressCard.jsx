99197bdf7cb30784c2e4e77bce9800a8b23b732f
hazwanihanafi-commits <hazwanihanafi@gmail.com>
2025-11-28 23:00:08 +0800

    Create ProgressCard.jsx

---
+++ frontend/components/ProgressCard.jsx
@@ -0,0 +1,9 @@
+// components/ProgressCard.jsx
+export default function ProgressCard({ title, value }) {
+  return (
+    <div className="rounded-xl bg-white p-4 shadow text-center">
+      <div className="text-sm text-gray-500">{title}</div>
+      <div className="text-2xl font-bold mt-2">{value}</div>
+    </div>
+  );
+}
