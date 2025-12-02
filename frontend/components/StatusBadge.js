c35cbee374fced1480bee4f2fbd8392b5cd46aec
hazwanihanafi-commits <hazwanihanafi@gmail.com>
2025-12-02 20:57:14 +0800

    Update StatusBadge.js

--- frontend/components/StatusBadge.js
+++ frontend/components/StatusBadge.js
@@ -1,10 +1,11 @@
+// frontend/components/StatusBadge.js
 export default function StatusBadge({ status }) {
   const color =
     status === "Completed"
       ? "bg-green-100 text-green-700"
-      : status === "On Track"
-      ? "bg-blue-100 text-blue-700"
-      : "bg-red-100 text-red-700"; // Late
+      : status === "Late"
+      ? "bg-red-100 text-red-700"
+      : "bg-blue-100 text-blue-700"; // On Track
 
   return (
     <span className={`px-3 py-1 rounded-full text-xs font-semibold ${color}`}>
