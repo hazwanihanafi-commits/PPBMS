b8e6c18cd625202028e7b728f8a82bfa556d6896
hazwanihanafi-commits <hazwanihanafi@gmail.com>
2025-12-02 08:06:07 +0800

    Update SupervisorStudentTable.jsx

--- frontend/components/SupervisorStudentTable.jsx
+++ frontend/components/SupervisorStudentTable.jsx
@@ -9,24 +9,31 @@ export default function SupervisorStudentTable({ students = [] }) {
           <th className="p-2">Name</th>
           <th className="p-2">Programme</th>
           <th className="p-2">Progress</th>
-          <th className="p-2">Status</th>
           <th className="p-2">Action</th>
         </tr>
       </thead>
+
       <tbody>
         {students.map(s => (
-          <tr key={s.id} className="border-t">
+          <tr key={s.email} className="border-t">
             <td className="p-2">{s.name}</td>
             <td className="p-2">{s.programme || "-"}</td>
             <td className="p-2">{s.progress}%</td>
-            <td className="p-2">{s.status}</td>
             <td className="p-2">
-              <Link href={`/supervisor/${encodeURIComponent(s.id)}`}>
+              <Link href={`/supervisor/${encodeURIComponent(s.email)}`}>
                 <a className="text-purple-600 hover:underline">Open</a>
               </Link>
             </td>
           </tr>
         ))}
+
+        {!students.length && (
+          <tr>
+            <td className="p-2 text-center" colSpan="4">
+              No students found.
+            </td>
+          </tr>
+        )}
       </tbody>
     </table>
   );
