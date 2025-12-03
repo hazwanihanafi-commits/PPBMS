8d6614c448fb85c02be6c7913321215184aedc7c
hazwanihanafi-commits <hazwanihanafi@gmail.com>
2025-12-03 22:05:56 +0800

    Update api.js

--- frontend/utils/api.js
+++ frontend/utils/api.js
@@ -1,1 +1,24 @@
-export const API_BASE = process.env.NEXT_PUBLIC_API_BASE;
+const API = process.env.NEXT_PUBLIC_API_BASE;
+
+export async function apiGet(path) {
+  const token = localStorage.getItem("token") || "";
+  const res = await fetch(`${API}${path}`, {
+    headers: { Authorization: `Bearer ${token}` }
+  });
+  return res.json();
+}
+
+export async function apiPost(path, body) {
+  const token = localStorage.getItem("token") || "";
+  const res = await fetch(`${API}${path}`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${token}`
+    },
+    body: JSON.stringify(body)
+  });
+  return res.json();
+}
+
+export const API_BASE = API;
