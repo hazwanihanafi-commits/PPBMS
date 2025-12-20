e2d5b3cd753223111dbf20d93b789ccd711e9025
hazwanihanafi-commits <hazwanihanafi@gmail.com>
2025-12-20 22:22:48 +0800

    Update supervisor.js

--- backend/routes/supervisor.js
+++ backend/routes/supervisor.js
@@ -8,7 +8,7 @@ import {
 
 import { buildTimelineForRow } from "../utils/buildTimeline.js";
 import { deriveCQIByAssessment } from "../utils/cqiAggregate.js";
-import { writeSheetCell } from "../services/googleSheets.js";
+import { updateASSESSMENT_PLO_Remark } from "../services/googleSheets.js";
 
 const router = express.Router();
 
