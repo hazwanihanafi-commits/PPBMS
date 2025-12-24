e120b425325b3abdb1535fa59abdc415737ed7ee
hazwanihanafi-commits <hazwanihanafi@gmail.com>
2025-12-24 14:36:30 +0800

    Update next.config.js

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,5 +1,6 @@
 /** @type {import('next').NextConfig} */
-module.exports = {
-  output: "export",
-  images: { unoptimized: true },
+const nextConfig = {
+  reactStrictMode: true
 };
+
+module.exports = nextConfig;
