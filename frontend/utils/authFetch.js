f096437a830b9b19e7e95f312160bdd3668eecd2
hazwanihanafi-commits <hazwanihanafi@gmail.com>
2025-12-28 12:17:22 +0800

    Update authFetch.js

--- frontend/utils/authFetch.js
+++ frontend/utils/authFetch.js
@@ -1,23 +1,15 @@
+// frontend/utils/authFetch.js
 import { API_BASE } from "./api";
 
 export async function authFetch(url, options = {}) {
   const token = localStorage.getItem("ppbms_token");
 
-  const res = await fetch(`${API_BASE}${url}`, {
+  return fetch(`${API_BASE}${url}`, {
     ...options,
     headers: {
+      ...(options.headers || {}),
+      Authorization: `Bearer ${token}`,
       "Content-Type": "application/json",
-      ...(token ? { Authorization: `Bearer ${token}` } : {}),
-      ...(options.headers || {})
-    }
+    },
   });
-
-  if (res.status === 401) {
-    localStorage.removeItem("ppbms_token");
-    localStorage.removeItem("ppbms_role");
-    window.location.href = "/login";
-    throw new Error("Unauthorized");
-  }
-
-  return res;
 }
