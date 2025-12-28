f6c7838c9e2679f71f4fbdc441b241490a0a8f8d
hazwanihanafi-commits <hazwanihanafi@gmail.com>
2025-12-28 15:58:18 +0800

    Create columnUtils.js

---
+++ backend/utils/columnUtils.js
@@ -0,0 +1,9 @@
+export function columnNumberToLetter(n) {
+  let s = "";
+  while (n > 0) {
+    let m = (n - 1) % 26;
+    s = String.fromCharCode(65 + m) + s;
+    n = Math.floor((n - m) / 26);
+  }
+  return s;
+}
