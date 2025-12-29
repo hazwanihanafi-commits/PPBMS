90d441e960c01582037b1ac8ade06955715b85b9
hazwanihanafi-commits <hazwanihanafi@gmail.com>
2025-12-29 14:17:14 +0800

    Update sendEmail.js

--- backend/services/sendEmail.js
+++ backend/services/sendEmail.js
@@ -3,9 +3,16 @@ import sgMail from "@sendgrid/mail";
 
 sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
-export default async function sendEmail({ to, subject, text, html }) {
+export default async function sendEmail({
+  to,
+  cc = [],
+  subject,
+  text,
+  html,
+}) {
   return sgMail.send({
     to,
+    cc,
     from: process.env.EMAIL_FROM,
     subject,
     text,
