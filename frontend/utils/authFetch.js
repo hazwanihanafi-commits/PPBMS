c3e17f9cae890f7473760aec810ad9ac62a9ea46
hazwanihanafi-commits <hazwanihanafi@gmail.com>
2025-12-29 16:57:49 +0800

    Update authFetch.js

--- frontend/utils/authFetch.js
+++ frontend/utils/authFetch.js
@@ -1,8 +1,9 @@
-// frontend/utils/authFetch.js
-import { API_BASE } from "./api";
-
 export async function authFetch(url, options = {}) {
   const token = localStorage.getItem("ppbms_token");
+
+  if (!token) {
+    throw new Error("NO_TOKEN");
+  }
 
   return fetch(`${API_BASE}${url}`, {
     ...options,
