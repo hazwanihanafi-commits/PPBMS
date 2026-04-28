3595b2bfc85314318cd579492559d4138008b66c
hazwanihanafi-commits <hazwanihanafi@gmail.com>
2026-04-28 16:34:04 +0800

    Update [email].js

--- frontend/pages/supervisor/[email].js
+++ frontend/pages/supervisor/[email].js
@@ -137,7 +137,6 @@ export default function SupervisorStudentPage() {
         {
           headers: {
             Authorization: `Bearer ${token}`,
-            "Cache-Control": "no-cache"
           },
         }
       );
