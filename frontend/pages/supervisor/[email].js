4ba9be56f6a762598b4fb2c6699f17614a0e3139
hazwanihanafi-commits <hazwanihanafi@gmail.com>
2026-05-05 09:10:45 +0800

    Update [email].js

--- frontend/pages/supervisor/[email].js
+++ frontend/pages/supervisor/[email].js
@@ -37,7 +37,7 @@ export default function SupervisorStudentPage() {
       const token = localStorage.getItem("ppbms_token");
 
       const res = await fetch(
-        `${API_BASE}/api/supervisor/student/${encodeURIComponent(email)}`,
+        `${API_BASE}/api/cqi/supervisor/student/${encodeURIComponent(email)}`,
         {
           headers: { Authorization: `Bearer ${token}` }
         }
